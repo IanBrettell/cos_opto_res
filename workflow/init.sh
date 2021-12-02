bad7fb34e3cfa28721f6f0444b5ca1c78b367aba
brettellebi <brettell@ebi.ac.uk>
2021-12-02 23:58:42 +0000

    20211203 update from Codon

--- workflow/init.sh
+++ workflow/init.sh
@@ -39,7 +39,7 @@ module load singularity-3.7.0-gcc-9.3.0-dp5ffrp
 PROJ=cos_opto_res
 
 ## Set container path
-CONT=/hps/software/users/birney/ian/containers/$PROJ/R_4.1.0.sif
+CONT=/hps/nobackup/birney/users/ian/containers/$PROJ/R_4.1.0.sif
 
 ## Build
 singularity build --remote \
@@ -51,7 +51,7 @@ ssh proxy-codon
 bsub -M 20000 -Is bash
 module load singularity-3.7.0-gcc-9.3.0-dp5ffrp
 PROJ=cos_opto_res
-CONT=/hps/software/users/birney/ian/containers/$PROJ/R_4.1.0.sif
+CONT=/hps/nobackup/birney/users/ian/containers/$PROJ/R_4.1.0.sif
 singularity shell --bind /hps/software/users/birney/ian/rstudio_db:/var/lib/rstudio-server \
                   --bind /hps/software/users/birney/ian/tmp:/tmp \
                   --bind /hps/software/users/birney/ian/run:/run \
